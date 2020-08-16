938a371c7e3a69b02066ab008995b2f0a5818d2f
kalindu-laksahan <kalilaksahan@gmail.com>
2020-08-16 19:41:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/comments/UpdateDeleteComment.java
@@ -0,0 +1,14 @@
+package com.example.comments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateDeleteComment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_delete_comment);
+    }
+}
